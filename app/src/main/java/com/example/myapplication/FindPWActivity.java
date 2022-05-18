c96fc9b980134529c67a03966941f29c8c50c46f
seoil <blesssy28@naver.com>
2022-05-18 15:57:03 +0900

    commit sex

--- app/src/main/java/com/example/myapplication/FindPWActivity.java
+++ app/src/main/java/com/example/myapplication/FindPWActivity.java
@@ -1,19 +1,14 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class idSearch extends AppCompatActivity {
+public class FindPWActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_id_search);
-
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-
+        setContentView(R.layout.activity_find_pwactivity);
     }
 }
--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -11,7 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button btn01, btn02;
+    Button btn01, btn02, btn03, btn04;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +26,31 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         btn02 = (Button) findViewById(R.id.store_sign_Btn);
         btn02.setOnClickListener(this);
+
+        btn03 = (Button) findViewById(R.id.idPwBtn);
+        btn03.setOnClickListener(this);
+
+        btn04 = (Button) findViewById(R.id.idPwBtn2);
+        btn04.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         if(view.getId() == R.id.signBtn) {
-            Intent intent = new Intent(this, SignActivity.class);
-            startActivity(intent);
+            Intent intent01 = new Intent(this, SignActivity.class);
+            startActivity(intent01);
         }
         if(view.getId() == R.id.store_sign_Btn) {
-            Intent intent01 = new Intent(this, StoreActivity.class);
-            startActivity(intent01);
+            Intent intent02 = new Intent(this, StoreActivity.class);
+            startActivity(intent02);
+        }
+        if(view.getId()==R.id.idPwBtn){
+            Intent intent03 = new Intent(this,FindIdActivuty.class);
+            startActivity(intent03);
+        }
+        if(view.getId()==R.id.idPwBtn2){
+            Intent intent04 = new Intent(this,FindPWActivity.class);
+            startActivity(intent04);
         }
     }
 }
