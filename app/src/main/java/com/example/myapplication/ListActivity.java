5b4f6727f089178a3e74062c407b0336b2346b04
mh <ymh3444@naver.com>
2022-05-19 18:00:25 +0900

    519 ㅇㅁㅎ

--- app/src/main/java/com/example/myapplication/ListActivity.java
+++ app/src/main/java/com/example/myapplication/ListActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -17,7 +20,19 @@ public class ListActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+        
+        /*테스트를 위한 더미데이터*/
+        ArrayList<String> testDataSet = new ArrayList<>();
+        for(int i = 0; i<20; i++){
+            testDataSet.add("Test Data" + i );
+        }
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager((Context) this);
+        recyclerView.setLayoutManager(linearLayoutManager); //layout매니저 설정
 
+        CustomAdapter customAdapter = new CustomAdapter(testDataSet);
+        recyclerView.setAdapter(customAdapter);
 
 
     }
