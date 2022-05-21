d3559ee3c3b3ca06ef9e95f4de403932bbad13b2
chane4266 <gta3gta2@gmail.com>
2022-05-21 16:18:16 +0900

    05.21 마이페이지 화면 수정 ㅇㅈㅊ

--- app/src/main/java/com/example/myapplication/MyPageActivity.java
+++ app/src/main/java/com/example/myapplication/MyPageActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -14,10 +16,13 @@ public class MyPageActivity extends AppCompatActivity implements View.OnClickLis
     private View thirdView; //내리뷰
 
     Button btn01, btn02, btn03;
+    RecyclerView fa_list;
+    StoreListAdapter list_adapter;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
 
@@ -33,6 +38,18 @@ public class MyPageActivity extends AppCompatActivity implements View.OnClickLis
         btn01.setOnClickListener(this);
         btn02.setOnClickListener(this);
         btn03.setOnClickListener(this);
+
+        fa_list = findViewById(R.id.favorite_list);
+        LinearLayoutManager lM = new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.VERTICAL, false);
+        fa_list.setLayoutManager(lM);
+
+        list_adapter = new StoreListAdapter(getApplicationContext());
+        fa_list.setAdapter(list_adapter);
+
+        list_adapter.addItem(new StoreListitem(R.drawable.moms,"맘스터치","서일대점","4.3"));
+        list_adapter.addItem(new StoreListitem(R.drawable.gomtang,"닭곰탕","서일대점","4.7"));
+        list_adapter.addItem(new StoreListitem(R.drawable.gomtang,"치즈밥","서일대점","4.4"));
+        list_adapter.addItem(new StoreListitem(R.drawable.ddug,"지지고","서일대점","4.9"));
     }
 
     public void init(){
